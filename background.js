406a17dfd03ee468f4a1bd6dfb72ddfabacdd0bd
Gabriella <68099900+sue614@users.noreply.github.com>
2021-09-05 23:25:43 +0900

    Update background.js

--- background.js
+++ background.js
@@ -11,20 +11,11 @@ const images = [
 
 
 const todayImage = images[Math.floor(Math.random()*images.length)];
-//이미지를 랜덤숫자로 호출할 수 있도록 선언해주고, floor 사용해 소수점뒤의 숫자 발생 시 숫자를 내림해준다.
-
 
 const bgImage = document.createElement("img");
-// 자바스크립트에서 hmtl에 요소(elements)를 추가해준다. 
-// 이렇게 코드를 작성하면 브라우정[서 실행 시, <img>태그가 생긴다.
-
 
 bgImage.src = `img/${todayImage}`;
-// 생성된 <img>태그에 이미지 소스를 추가한다.
 
 document.body.appendChild(bgImage);
-// appendChild를 사용해 body에 html을 추가한다.
-// append나 prepend를 사용해도 가능하고, append를 사용하면 body 맨뒤에
-// 위치하고, prepend 사용하면 body 맨윗줄로 오게된다.
 
-element.style.background: `url("https://sue614.github.io/Momentum/src/img/${list[idx]}")`
+//element.style.background: `url("https://sue614.github.io/Momentum/src/img/${list[idx]}")`
